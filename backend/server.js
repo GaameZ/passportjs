05c865941432b184e397cd35f0c1dfd0ffeebd76
Alexy Samyn <alexy59430@gmail.com>
2019-07-09 16:02:59 +0200

    inscription ok, password hash

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const app = express();
-const { portNumber } = require("./conf.js");
+
+const { portNumber, db, saltRounds } = require("./conf.js");
+
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Works !");
+  res.send("/");
+});
+
+app.post("/signup", (req, res) => {
+  const email = req.body.email;
+  let password = req.body.password;
+  const pseudo = req.body.pseudo;
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    if (err) console.log(err);
+    password = hash;
+
+    db.query(
+      "INSERT INTO users (email, password, pseudo) values (?,?,?)",
+      [email, password, pseudo],
+      (err, results) => {
+        if (err) {
+          res.status(500).send("Erreur");
+        } else {
+          res.sendStatus(200);
+        }
+      }
+    );
+  });
 });
 
 app.listen(portNumber, () => {
